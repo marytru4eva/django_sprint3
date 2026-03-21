e7536e318aa23e7c3c6020ccdf12ad76f9f10b43
MaryTru4eva <marytru4eva@yandex.com>
2026-03-21 14:09:39 +0700

    Fix urls and views for pages

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
 def index(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.filter(
+        is_published=True,
+        category__is_published=True,
+        pub_date__isnull=False
+    ).order_by('-pub_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(
+        Post.objects.filter(
+            is_published=True,
+            category__is_published=True,
+            pub_date__isnull=False
+        ),
+        id=post_id
+    )
+    return render(request, 'blog/detail.html', {'post': post})
+
+def category_posts(request, category_slug):
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    posts = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__isnull=False
+    ).order_by('-pub_date')
+    return render(request, 'blog/category.html', {'category': category, 'posts': posts})
