8419e8303a09d7c7341de71b34cce908f19833ad
MaryTru4eva <marytru4eva@yandex.com>
2026-03-21 14:35:38 +0700

    Fix PEP8 line too long in urls.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path(
+        'category/<slug:category_slug>/',
+        views.category_posts,
+        name='category_posts'
+    ),
 ]
