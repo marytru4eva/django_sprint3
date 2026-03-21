302f3661bf0c42a04ad50e7133ce6a3aa0f421ba
MaryTru4eva <marytru4eva@yandex.com>
2026-03-21 13:13:49 +0700

    Add models Post, Category, Location

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,1 +1,45 @@
-# Create your models here.
+from django.db import models
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=256)
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    pub_date = models.DateTimeField()
+    author = models.CharField(max_length=64)
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
