56bd67afa5177d0516ca6cfadc42a49058bf6536
MaryTru4eva <marytru4eva@yandex.com>
2026-04-01 19:58:58 +0700

    Sprint 3 project

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Post
+from .models import Location
+from .models import Category
+
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Location)
---
+++ blogicum/blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import get_object_or_404, render
+from blog.models import Post, Category
+from datetime import datetime
+
+
+def index(request):
+    template = 'blog/index.html'
+    post_list = Post.objects.all().filter(
+        is_published=True, pub_date__lte=datetime.now(),
+        category__is_published=True).order_by('title')[0:5]
+    context = {'post_list': post_list}
+    return render(request, template, context)
+
+
+def post_detail(request, pk):
+    template = 'blog/detail.html'
+    post = get_object_or_404(Post.objects.all(),
+                             pk=pk,
+                             is_published=True,
+                             pub_date__lte=datetime.now(),
+                             category__is_published=True)
+    context = {'post': post}
+    return render(request, template, context)
+
+
+def category_posts(request, category_slug):
+    template = 'blog/category.html'
+    category = get_object_or_404(Category.objects.all(), slug=category_slug,
+                                 is_published=True)
+    post_list = Post.objects.all().filter(
+        is_published=True, pub_date__lte=datetime.now(), category=category)
+    context = {'category': category,
+               'post_list': post_list}
+    return render(request, template, context)
